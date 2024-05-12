bca0f6a414bfda81c565c4571ada649de8ed3c2e
LoreDiGiovanni <lorenzo.di.giovanni00@gmail.com>
2024-05-12 23:50:04 +0200

    open pdf in current dir

--- lua/base/remap.lua
+++ lua/base/remap.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n', '<leader>ee', ":bprev<CR>", {})
 vim.keymap.set('n', '<leader>qq', ":bnext<CR>", {})
 vim.keymap.set('n', '<leader>sc', ":setlocal spell<CR>", {})
 vim.keymap.set('n', '<leader>lc', ":!pdflatex %:r.tex ; rm *.aux *.log *.toc<CR>",{})
+vim.keymap.set('n', '<leader>op', ":!zathura %:r.pdf &<CR>",{})
 vim.keymap.set('n', '<leader>mr', ":!make run <CR>",{})
 
 -- nvim candy
@@ -13,6 +14,7 @@ vim.keymap.set("n", "<c-d>", "<c-d>zz")
 vim.keymap.set("n", "<c-u>", "<c-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
 
 
 

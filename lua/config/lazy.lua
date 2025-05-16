b1e6b89bef722de6eacb852a36a95f3cd11c34f4
typegaro <lorenzo.di.giovanni00@gmail.com>
2025-05-16 17:26:53 +0200

    bubu update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -47,31 +47,40 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.clipboard="unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 vim.opt.spelllang = "it_it,en_us"
 vim.o.termguicolors = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-    {'nvim-telescope/telescope.nvim'},
-    { "NTBBloodbath/doom-one.nvim", config = function() vim.cmd.colorscheme "doom-one" end },
-    { "nvim-treesitter/nvim-treesitter" },
-    { "theprimeagen/harpoon" },
-    { "mbbill/undotree" },
-    { "tpope/vim-fugitive" },
-    { "neovim/nvim-lspconfig" },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-nvim-lua' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    {'lewis6991/gitsigns.nvim', config = function() require('gitsigns').setup() end },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  --install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  --checker = { enabled = true },
+    spec = {
+        { import = "plugins" },
+        { 'nvim-telescope/telescope.nvim' },
+        { "NTBBloodbath/doom-one.nvim",     config = function() vim.cmd.colorscheme "doom-one" end },
+        { "nvim-treesitter/nvim-treesitter" },
+        { "theprimeagen/harpoon" },
+        { "mbbill/undotree" },
+        { "tpope/vim-fugitive" },
+        { "neovim/nvim-lspconfig" },
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'hrsh7th/cmp-buffer' },
+        { 'hrsh7th/cmp-path' },
+        { 'hrsh7th/cmp-nvim-lua' },
+        { 'saadparwaiz1/cmp_luasnip' },
+        { 'lewis6991/gitsigns.nvim',        config = function() require('gitsigns').setup() end },
+        {
+            "iamcco/markdown-preview.nvim",
+            cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+            build = "cd app && yarn install",
+            init = function()
+                vim.g.mkdp_filetypes = { "markdown" }
+            end,
+            ft = { "markdown" },
+        },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    --install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    --checker = { enabled = true },
 })
